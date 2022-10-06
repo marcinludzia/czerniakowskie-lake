bab12b7d1c5ad3d339739848c6d846f4164c71b7
marcinludzia <marcin.ludzia@gmail.com>
2022-10-07 00:05:16 +0200

    Fix in installation script

--- scripts/install_on_test.sh
+++ scripts/install_on_test.sh
@@ -1,4 +1,11 @@
-rm -R /home/marcin/applications/czerniakowskie-lake/*
+# remove old version
+rm -R /home/marcin/applications/czerniakowskie-lake/app/bin
+rm -R /home/marcin/applications/czerniakowskie-lake/app/lib
+rm /home/marcin/applications/czerniakowskie-lake/app.zip
+
+# installation of the application
 cp /home/marcin/actions-runner/_work/czerniakowskie-lake/czerniakowskie-lake/app/build/distributions/app.zip /home/marcin/applications/czerniakowskie-lake/
 cd /home/marcin/applications/czerniakowskie-lake/ && unzip app.zip
+
+# running the application
 cd /home/marcin/applications/czerniakowskie-lake/app/bin && ./app
