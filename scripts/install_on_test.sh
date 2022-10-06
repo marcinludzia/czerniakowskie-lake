bf0bf0941c66a42b4f400df079a7748731771749
marcinludzia <marcin.ludzia@gmail.com>
2022-10-07 01:07:30 +0200

    Added service stop

--- scripts/install_on_test.sh
+++ scripts/install_on_test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# stopping currently running application
+echo "Stopping the czerniakowskie-lake app..."
+kill $(ps -ef | grep '/home/marcin/applications/czerniakowskie-lake/app/lib/app.jar' | grep -v 'grep' | awk '{printf $2}')
+
 # remove old version
 rm -R /home/marcin/applications/czerniakowskie-lake/app/bin
 rm -R /home/marcin/applications/czerniakowskie-lake/app/lib
@@ -9,10 +13,7 @@ rm /home/marcin/applications/czerniakowskie-lake/app.zip
 cp /home/marcin/actions-runner/_work/czerniakowskie-lake/czerniakowskie-lake/app/build/distributions/app.zip /home/marcin/applications/czerniakowskie-lake/
 cd /home/marcin/applications/czerniakowskie-lake/ && unzip app.zip
 
-# stopping currently running application
-echo "Stopping the czerniakowskie-lake app..."
-kill $(ps -ef | grep '/home/marcin/applications/czerniakowskie-lake/app/lib/app.jar' | grep -v 'grep' | awk '{printf $2}')
-
 # running the application
 echo "Starting czerniakowskie-lake app..."
 cd /home/marcin/applications/czerniakowskie-lake/app/bin && ./app &
+echo $!
